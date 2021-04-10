04e13092397cbf2f7a13262a80ce10041d8c5bbc
fernandafons <nanda.fonseca7@gmail.com>
2021-04-10 20:35:00 -0300

    Readme update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # SearchRestaurantApp
+
+This is an app where you can search for restaurants all around the world. I started developing it during the [Complete React Native + Hooks Course](https://www.udemy.com/course/the-complete-react-native-and-redux-course/), but I decided to go further by myself and develop other options and features that were not part of the course. It was interesting making an API connection in React. 
+----------------------------
+
+## Getting Started
+
+1. Clone this project on your computer.
+2. Run `npm run start` back in your blog directory
+----------------------------
+
+## Built With
+
+* [React Native](https://reactnative.dev/) - The framework used
+* [Yelp](https://www.yelp.com/developers) - The API used
+
+## Authors
+
+* **Maria Fernanda** - *Initial work* - [FernandaFons](https://github.com/fernandafons)
+
+Any questions? Contact me on my [LinkedIn](https://www.linkedin.com/in/fernandafons/)
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,8 +8,6 @@ const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
-
-
     const filterResultsByPrice = (price) => {
         return results.filter(result => {
             return result.price === price;
@@ -17,30 +15,28 @@ const SearchScreen = () => {
     };
 
     return (
-        // <View style={{ flex: 1 }} >
-        <>
-            <SearchBar 
-                term={term} 
-                onTermChange={setTerm}
-                onTermSubmit={() => searchApi(term)} 
-            />
-            {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <ScrollView>
-            <ResultsList 
-                results={filterResultsByPrice('$')} 
-                title="Cost Effective"
-            />
-            <ResultsList 
-                results={filterResultsByPrice('$$')} 
-                title="Bit Pricier"
-            />
-            <ResultsList 
-                results={filterResultsByPrice('$$$')} 
-                title="Big Spender"
-            />
-            </ScrollView>
-        </>
-        // </View>
+        <View>
+        <SearchBar 
+            term={term} 
+            onTermChange={setTerm}
+            onTermSubmit={() => searchApi(term)} 
+        />
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
+        <ScrollView>
+        <ResultsList 
+            results={filterResultsByPrice('$')} 
+            title="Cost Effective"
+        />
+        <ResultsList 
+            results={filterResultsByPrice('$$')} 
+            title="Bit Pricier"
+        />
+        <ResultsList 
+            results={filterResultsByPrice('$$$')} 
+            title="Big Spender"
+        />
+        </ScrollView>
+        </View>
     );
 };
 
